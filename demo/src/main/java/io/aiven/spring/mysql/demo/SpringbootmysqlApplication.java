c6e159c4073b219eb80321861a1740671a53ccde
MadanGithub30 <97211958+MadanGithub30@users.noreply.github.com>
2024-01-08 11:10:25 +0530

    Initial commit

---
+++ demo/src/main/java/io/aiven/spring/mysql/demo/SpringbootmysqlApplication.java
@@ -0,0 +1,14 @@
+package io.aiven.spring.mysql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmysqlApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootmysqlApplication.class, args);
+	}
+
+}
